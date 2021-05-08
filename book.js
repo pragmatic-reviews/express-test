df9276d4e6461056fdc80400b2b228eab2c83f25
pragmaticreviews <info@pragmaticreviews.com>
2021-05-08 10:00:18 -0300

    Adding comments

--- book.js
+++ book.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-// middleware that is specific to this router
+// Sample middleware
 router.use(function timeLog (req, res, next) {
   console.log('Time: ', Date.now());
   next();
 })
-// define the home page route
+// GET /book
 router.get('/book', function (req, res) {
   res.send('Get a book');
 })
-// define the about route
+// POST /book
 router.post('/book', function (req, res) {
   res.send('Add a book');
 })
