9db72bc920fcca9f0f831da5fcbeee869fb536ef
pragmaticreviews <info@pragmaticreviews.com>
2021-05-22 11:18:48 -0300

    Replacing var with const

--- routes/books.js
+++ routes/books.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+
 
 // Time Middleware
 const time = require("../middlewares/time.js");
